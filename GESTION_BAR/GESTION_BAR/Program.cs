d97d8c7cd69f04bb2f1ef0f6e20dca324721c8f4
PhoVic14 <91669693+PhoVic14@users.noreply.github.com>
2024-05-27 22:55:07 +0200

    AJout du bar avec BDD
    Ajout du bar avec BDD

---
+++ GESTION_BAR/GESTION_BAR/Program.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.Diagnostics;
+using Google.Protobuf.WellKnownTypes;
+using System.Collections;
+using System.Data.Common;
+using System.Security.Cryptography.X509Certificates;
+using MySqlX.XDevAPI.Relational;
+
+namespace GESTION_BAR
+{
+    internal class Program
+    {
+        static string DefinirCheminBD()
+        {
+            string server = "localhost";
+            string database = "barcocktail";
+            string uid = "root";
+            string password = "root";
+            string connectionString;
+
+            return "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+        }
+
+        static bool ChercherInfos(string table, out DataSet infos)
+        {
+            MySqlConnection maConnection = new MySqlConnection(DefinirCheminBD());
+            bool ok = true;
+            string query = "";
+            try
+            {
+                maConnection.Open();
+
+                query = "select * from " + table + ";";
+
+                MySqlDataAdapter da = new MySqlDataAdapter(query, maConnection);
+                infos = new DataSet();
+                da.Fill(infos, "table");
+                maConnection.Close();
+                return ok;
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                throw;
+            }
+        }
+
+        public bool ChercheRecetteCocktail(int numCocktail, out DataSet recettes)
+        {
+            bool ok = false;
+            MySqlConnection maConnection = new MySqlConnection(DefinirCheminBD());
+            string query = "";
+            try
+            {
+                maConnection.Open();
+
+                query = "SELECT * FROM Recettes WHERE cocktailId=" + numCocktail + ";";
+
+                MySqlDataAdapter da = new MySqlDataAdapter(query, maConnection);
+                recettes = new DataSet();
+                da.Fill(recettes, "infos");
+
+                if (recettes.Tables[0].Rows.Count >= 1)
+                {
+                    ok = true;
+                }
+                maConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                throw;
+            }
+
+            return ok;
+        }
+
+    }
+}
---
+++ GESTION_BAR/GESTION_BAR/bar.sql
@@ -0,0 +1,52 @@
+﻿CREATE DATABASE IF NOT EXISTS BarCocktail;
+USE BarCocktail;
+
+CREATE TABLE RECETTE
+(
+    recetteId INT PRIMARY KEY auto_increment NOT NULL,
+    recetteQuantite double not null,
+    liquideId int not null,
+    cocktailId int not null,
+    FOREIGN KEY (liquideId) REFERENCES LIQUIDES(liquideId),
+    FOREIGN KEY (cocktailId) REFERENCES COCKTAILS(cocktailId)
+);
+
+CREATE TABLE LIQUIDES
+(
+    liquideId INT PRIMARY KEY auto_increment NOT NULL,
+    liquideNom VARCHAR(30) not null,
+    liquideType VARCHAR(15) not null
+);
+
+CREATE TABLE COCKTAILS
+(
+    cocktailId INT PRIMARY KEY auto_increment NOT NULL,
+    cocktailNom varchar(30) not null
+);
+
+-- Ajout de liquides supplémentaires
+INSERT INTO LIQUIDES (liquideNom, liquideType) VALUES
+('Vodka', 'Alcool'),
+('Rhum blanc', 'Alcool'),
+('Whisky', 'Alcool'),
+('Tequila', 'Alcool'),
+('Cointreau', 'Alcool'),
+('Gin', 'Alcool'),
+('Jus de citron', 'Jus'),
+('Jus ananas', 'Jus'),
+('Cranberry', 'Jus'),
+('Limonade', 'Soda'),
+('Grenadine', 'Sirop'),
+('Lait de coco', 'Lait');
+
+-- Ajout de cocktails supplémentaires
+INSERT INTO COCKTAILS (cocktailNom) VALUES
+('Cosmopolitan'),
+('Long Island Iced Tea'),
+('Whisky Sour'),
+('Tequila Sunrise'),
+('Gin Tonic');
+
+
+select * from cocktails;
+select * from liquides;
